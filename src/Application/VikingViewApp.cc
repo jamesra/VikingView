c4c4c81f62bb0ea2ee386ab3161a8023658e58bb
Alan Morris <akenmorris@gmail.com>
2015-01-21 23:48:53 -0700

    Adding some intial files.

---
+++ src/Application/VikingViewApp.cc
@@ -0,0 +1,170 @@
+// std
+#include <iostream>
+
+// qt
+#include <QFileDialog>
+#include <QWidgetAction>
+
+
+// vtk
+#include <vtkRenderWindow.h>
+
+// studio
+#include <Application/VikingViewApp.h>
+
+// ui
+#include <ui_VikingViewApp.h>
+
+//---------------------------------------------------------------------------
+VikingViewApp::VikingViewApp( int argc, char** argv )
+{
+  this->ui_ = new Ui_VikingViewApp;
+  this->ui_->setupUi( this );
+}
+
+//---------------------------------------------------------------------------
+VikingViewApp::~VikingViewApp()
+{}
+
+//---------------------------------------------------------------------------
+void VikingViewApp::on_add_button_clicked()
+{
+  bool ok;
+  QString text = QInputDialog::getText( this, tr( "Please enter an ID or list of IDs" ),
+                                        tr( "ID:" ), QLineEdit::Normal,
+                                        "", &ok );
+  if ( ok && !text.isEmpty() )
+  {
+
+    int id = text.toInt();
+    std::cerr << "id = " << id << "\n";
+    //textLabel->setText( text );
+    this->load_structure(id);
+  }
+}
+
+//---------------------------------------------------------------------------
+void VikingViewApp::on_delete_button_clicked()
+{}
+
+//---------------------------------------------------------------------------
+void VikingViewApp::load_structure(int id)
+{
+
+
+  QUrl url = "http://connectomes.utah.edu/Rabbit/OData/ConnectomeData.svc/Locations/?$filter=ParentID eq 476";
+
+  QNetworkRequest request = QNetworkRequest(url);
+
+  //request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+  //request.setRawHeader("Content-Type", "application/json");
+  request.setRawHeader("Accept", "application/json");
+
+  reply = qnam.get(request);
+
+
+  file = new QFile("C:\\Users\\amorris\\file.txt");
+
+
+  if (!file->open(QIODevice::WriteOnly)) {
+    QMessageBox::information(this, tr("HTTP"),
+      tr("Unable to save the file %1: %2.")
+      .arg("asdf").arg(file->errorString()));
+    delete file;
+    file = 0;
+    return;
+  }
+
+  this->httpRequestAborted = false;
+
+  connect(reply, SIGNAL(finished()),
+    this, SLOT(httpFinished()));
+  connect(reply, SIGNAL(readyRead()),
+    this, SLOT(httpReadyRead()));
+  connect(reply, SIGNAL(downloadProgress(qint64,qint64)),
+    this, SLOT(updateDataReadProgress(qint64,qint64)));
+
+}
+
+void VikingViewApp::httpFinished()
+{
+  std::cerr << "http finished!\n";
+
+  if (httpRequestAborted) {
+    if (file) {
+      file->close();
+      file->remove();
+      delete file;
+      file = 0;
+    }
+    reply->deleteLater();
+#ifndef Q_WS_MAEMO_5
+//    progressDialog->hide();
+#endif
+    return;
+  }
+
+#ifndef Q_WS_MAEMO_5
+  //progressDialog->hide();
+#endif
+  file->flush();
+  file->close();
+
+  QVariant redirectionTarget = reply->attribute(QNetworkRequest::RedirectionTargetAttribute);
+  if (reply->error()) {
+    file->remove();
+    QMessageBox::information(this, tr("HTTP"),
+      tr("Download failed: %1.")
+      .arg(reply->errorString()));
+    //downloadButton->setEnabled(true);
+  } else if (!redirectionTarget.isNull()) {
+    //QUrl newUrl = url.resolved(redirectionTarget.toUrl());
+    //if (QMessageBox::question(this, tr("HTTP"),
+    //  tr("Redirect to %1 ?").arg(newUrl.toString()),
+    //  QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes) {
+    //    url = newUrl;
+    //    reply->deleteLater();
+    //    file->open(QIODevice::WriteOnly);
+    //    file->resize(0);
+    //    startRequest(url);
+    //    return;
+  //}
+  } else {
+    //QString fileName = QFileInfo(QUrl(urlLineEdit->text()).path()).fileName();
+    //statusLabel->setText(tr("Downloaded %1 to %2.").arg(fileName).arg(QDir::currentPath()));
+    //downloadButton->setEnabled(true);
+  }
+
+  reply->deleteLater();
+  reply = 0;
+  delete file;
+  file = 0;
+}
+
+void VikingViewApp::httpReadyRead()
+{
+  std::cerr << "http read...\n";
+  // this slot gets called every time the QNetworkReply has new data.
+  // We read all of its new data and write it into the file.
+  // That way we use less RAM than when reading it at the finished()
+  // signal of the QNetworkReply
+  if (file)
+    file->write(reply->readAll());
+}
+
+void VikingViewApp::updateDataReadProgress(qint64 bytesRead, qint64 totalBytes)
+{
+  std::cerr << "http update...\n";
+
+  if (httpRequestAborted)
+    return;
+
+#ifndef Q_WS_MAEMO_5
+//  progressDialog->setMaximum(totalBytes);
+  //progressDialog->setValue(bytesRead);
+#else
+  Q_UNUSED(bytesRead);
+  Q_UNUSED(totalBytes);
+#endif
+}
---
+++ src/Application/VikingViewApp.h
@@ -0,0 +1,71 @@
+#ifndef VIKINGVIEW_APPLICATION_VIKINGVIEWAPP_H
+#define VIKINGVIEW_APPLICATION_VIKINGVIEWAPP_H
+
+#include <QMainWindow>
+#include <QActionGroup>
+#include <QSlider>
+#include <QLabel>
+#include <QTimer>
+
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QUrl>
+
+class Lightbox;
+class GroomTool;
+class OptimizeTool;
+class AnalysisTool;
+class Project;
+class Visualizer;
+
+// Forward Qt class declarations
+class Ui_VikingViewApp;
+
+class LightboxLayout;
+class WheelEventForwarder;
+
+//! Main VikingView window
+/*!
+ * This class represents the primary VikingView window interface
+ */
+class VikingViewApp : public QMainWindow
+{
+  Q_OBJECT
+public:
+
+  VikingViewApp( int argc, char** argv );
+  ~VikingViewApp();
+
+  //virtual void closeEvent( QCloseEvent* event );
+
+public Q_SLOTS:
+
+  void on_add_button_clicked();
+  void on_delete_button_clicked();
+
+
+  void httpFinished();
+  void httpReadyRead();
+  void updateDataReadProgress(qint64 bytesRead, qint64 totalBytes);
+
+
+
+
+private:
+
+
+  void load_structure( int id );
+
+  /// designer form
+  Ui_VikingViewApp* ui_;
+
+  QFile *file;
+  QNetworkAccessManager qnam;
+  QNetworkReply *reply;
+
+  bool httpRequestAborted;
+
+};
+
+#endif /* VIKINGVIEW_APPLICATION_VIKINGVIEWAPP_H */
