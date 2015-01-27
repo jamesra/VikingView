7c7c0b410c711e91ac3d10fdf6fa1679be3147b3
Alan Morris <akenmorris@gmail.com>
2015-01-26 23:53:47 -0700

    Forgot to add Data dir.

---
+++ src/Data/Json.h
@@ -0,0 +1,17 @@
+#include <QScriptValue>
+
+class Json
+{
+public:
+  Json();
+  ~Json();
+
+  static QString encode(const QMap<QString,QVariant> &map);
+  static QMap<QString, QVariant> decode(const QString &jsonStr);
+  static QScriptValue encodeInner(const QMap<QString,QVariant> &map, QScriptEngine* engine);
+  static QMap<QString, QVariant> decodeInner(QScriptValue object);
+  static QList<QVariant> decodeInnerToList(QScriptValue arrayValue);
+
+
+
+};
