7d037bfee836b2a132b746326054e7a508ae40dc
Alan Morris <akenmorris@gmail.com>
2015-02-05 10:09:04 -0700

    Fixed up cylinder sampling.

--- src/Data/PointSampler.cc
+++ src/Data/PointSampler.cc
@@ -65,10 +65,19 @@ std::list<Point> PointSampler::sample_points()
 
   // cylinders
 
+  //std::ofstream out;
+  //out.open( "C:\\Users\\amorris\\points.out" );
+
   int num_pts_circle = 5;
   int num_pts_line = 5;
   num_radii = 2;
   foreach( Link link, this->structure_->get_links() ) {
+
+    if ( node_map.find( link.a ) == node_map.end() || node_map.find( link.b ) == node_map.end() )
+    {
+      continue;
+    }
+
     Node n1 = node_map[link.a];
     Node n2 = node_map[link.b];
 
@@ -112,7 +121,7 @@ std::list<Point> PointSampler::sample_points()
     double this_num_circle = num_pts_circle;
     double this_num_radii = num_radii;
 
-    for ( double r = 0; r < this_num_radii; r++ )
+    for ( double r = 1; r <= this_num_radii; r++ )
     {
       double n1_radius = n1.radius * ( r / this_num_radii );
       double n2_radius = n2.radius * ( r / this_num_radii );
@@ -121,55 +130,38 @@ std::list<Point> PointSampler::sample_points()
       {
         double theta = (double)i * 2 * M_PI / this_num_circle;
 
-        // ...
+        double circ1_x = n1.x + n1_radius * cos( theta ) * u[0] + n1_radius * sin( theta ) * v[0];
+        double circ1_y = n1.y + n1_radius * cos( theta ) * u[1] + n1_radius * sin( theta ) * v[1];
+        double circ1_z = n1.z + n1_radius * cos( theta ) * u[2] + n1_radius * sin( theta ) * v[2];
+
+        double circ2_x = n2.x + n2_radius * cos( theta ) * u[0] + n2_radius * sin( theta ) * v[0];
+        double circ2_y = n2.y + n2_radius * cos( theta ) * u[1] + n2_radius * sin( theta ) * v[1];
+        double circ2_z = n2.z + n2_radius * cos( theta ) * u[2] + n2_radius * sin( theta ) * v[2];
+
+        for ( double j = 0; j < this_num_points; j++ )
+        {
+          double ratio = j / ( (double)this_num_points - 1 );
+          double inv_ratio = 1 - ratio;
+
+          double this_x = circ1_x * ratio + circ2_x * inv_ratio;
+          double this_y = circ1_y * ratio + circ2_y * inv_ratio;
+          double this_z = circ1_z * ratio + circ2_z * inv_ratio;
+
+          Point p( this_x, this_y, this_z );
+          points.push_back( p );
+
+          if ( this_x == 0 )
+          {
+            std::cerr << "oh no, break!\n";
+          }
+
+          //out << this_x << " " << this_y << " " << this_z << "\n";
+        }
       }
     }
-
-/*
-
-
-   for r in 1..this_num_radii
-   n1_radius = n1.radius * r.to_f / this_num_radii.to_f
-   n2_radius = n2.radius * r.to_f / this_num_radii.to_f
-
-
-   for i in 0..this_num_circle-1 do
-   theta = i.to_f * 2 * Math::PI / this_num_circle.to_f
-
-   circ1_x = n1.x + n1_radius*Math.cos(theta)*u[0] + n1_radius*Math.sin(theta)*v[0]
-   circ1_y = n1.y + n1_radius*Math.cos(theta)*u[1] + n1_radius*Math.sin(theta)*v[1]
-   circ1_z = n1.z + n1_radius*Math.cos(theta)*u[2] + n1_radius*Math.sin(theta)*v[2]
-
-   circ2_x = n2.x + n2_radius*Math.cos(theta)*u[0] + n2_radius*Math.sin(theta)*v[0]
-   circ2_y = n2.y + n2_radius*Math.cos(theta)*u[1] + n2_radius*Math.sin(theta)*v[1]
-   circ2_z = n2.z + n2_radius*Math.cos(theta)*u[2] + n2_radius*Math.sin(theta)*v[2]
-
-
-   for j in 0..this_num_points-1 do
-   ratio =  j.to_f / (this_num_points.to_f - 1)
-   opp_ratio = 1 - ratio
-
- #        this_x = n1.x * ratio + n2.x * opp_ratio
- #        this_y = n1.y * ratio + n2.y * opp_ratio
- #        this_z = n1.z * ratio + n2.z * opp_ratio
-
-   this_x = circ1_x * ratio + circ2_x * opp_ratio
-   this_y = circ1_y * ratio + circ2_y * opp_ratio
-   this_z = circ1_z * ratio + circ2_z * opp_ratio
-
-   p = Point.new
-   p.x = this_x
-   p.y = this_y
-   p.z = this_z
-   points.push(p)
-   end
-   end
-   end
-
-
-
- */
   }
+
+  // out.close();
 
   std::cerr << "Sampled " << points.size() << " points\n";
 
